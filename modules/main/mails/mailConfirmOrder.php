0999a1024ec6baf2c22b48e122c1977f18d36e72
support <support@reworkhub.com>
2019-04-23 11:37:15 +0300

    Added confirm mail

---
+++ modules/main/mails/mailConfirmOrder.php
@@ -0,0 +1,170 @@
+<?php
+/* @var $this yii\web\View */
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link href="https://fonts.googleapis.com/css?family=Oxygen:300,400,700" rel="stylesheet">
+    <title>ReWorkhub</title>
+
+    <style>
+        body{
+            height: 100%;
+        }
+        body,
+        html,
+        .body {
+            background-color: #fff!important;
+            margin: 0;
+        }
+        body, table.body, h1, h2, h3, h4, h5, h6, p, td, th, a{
+            font-family: 'Oxygen', sans-serif;
+            font-size: 16px;
+            line-height: 1.3;
+            color: #454545;
+        }
+        a{
+            -webkit-transition: color .2s ease-in-out;
+            -o-transition: color .2s ease-in-out;
+            transition: color .2s ease-in-out;
+        }
+        a:visited{
+            color: #454545;
+        }
+        a:hover{
+            color: #00b473;
+        }
+
+        @media only screen and (max-width: 620px) {
+            table.body{
+                width: 100%!important;
+
+            }
+            body, table.body, h1, h2, h3, h4, h5, h6, p, td, th, a{
+                font-size: 15px!important;
+            }
+            .wrap-columns{
+                padding: 10px!important;
+            }
+        }
+
+    </style>
+</head>
+
+<body>
+
+<table class="body" style="width: 620px; margin: 0 auto; background: url(http://cleancode.me/test/email-images/bg-green.png) no-repeat 100% 100%; background-size: contain; background-color: #fff; font-family: 'Oxygen', sans-serif; color: #454545;">
+    <tr>
+        <td style="width: 580px; margin: 0 auto; background: url(http://cleancode.me/test/email-images/circle.png) no-repeat 0 0; background-size: 50px 113px;">
+            <center data-parsed="">
+
+                <table style="width: 100%;">
+                    <tbody>
+                    <tr>
+                        <td>
+
+                            <table style="width: 100%;">
+                                <tbody>
+                                <tr>
+                                    <th class="wrap-columns" style="padding: 10px 65px;">
+                                        <table style="width: 100%;">
+                                            <tr>
+                                                <th style="background: #f9f9f9; padding: 20px; border-radius: 8px 30px 8px 8px; z-index: 1;">
+
+                                                    <table style="width: 100%;">
+                                                        <tbody>
+                                                        <tr>
+
+                                                            <th>
+                                                                <table style="width: 100%;">
+                                                                    <tr>
+                                                                        <th>
+                                                                            <p style="text-align: left; font-size: 16px; font-weight: normal; padding: 10px 0 0; margin: 0 0 10px;">Благодарим Вас за оставленную вакансию на нашем <a href="https://reworkhub.com/" style="font-weight: 700;">сайте.</a><br> После проверки Ваша вакансия будет добавлена в порядке очереди. </p>
+                                                                        </th>
+                                                                    </tr>
+                                                                </table>
+                                                            </th>
+
+                                                            <th>
+                                                                <table style="width: 100%;">
+                                                                    <tr>
+                                                                        <th>
+                                                                            <p style="text-align: right; margin: -10px 0 0;"><a href="https://reworkhub.com/"><img src="https://reworkhub.com/images/logo.png" class="logo" style="height: 100px; width: 100px; display: inline-block; vertical-align: top;"></a></p>
+                                                                        </th>
+                                                                    </tr>
+                                                                </table>
+                                                            </th>
+
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+
+                                                    <table class="border-bottom" style="background: linear-gradient(90deg,#00b473 0,transparent 100%); width: 100%; font-size:1px;line-height:1px; height: 1px;"></table>
+
+                                                    <table>
+                                                        <tbody>
+                                                        <tr>
+                                                            <td height="10px" style="font-size:10px;line-height:10px;"></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+
+                                                    <center><p><img src="https://reworkhub.com/images/checked.png" align="center" style="height: 200px; width: 200px;"></p></center>
+
+                                                    <table>
+                                                        <tbody>
+                                                        <tr>
+                                                            <td height="10px" style="font-size:10px; line-height:10px;"></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+
+                                                    <center style="font-size: 16px; font-weight: normal"><a href="https://reworkhub.com/" style="font-weight: 700;">вернуться</a></center>
+
+                                                    <table>
+                                                        <tbody>
+                                                        <tr>
+                                                            <td height="20px" style="font-size:20px;line-height:20px;"></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+
+                                                    <center>
+                                                        <table>
+                                                            <tr>
+                                                                <td>
+                                                                    <table>
+                                                                        <tr>
+                                                                            <th><a href="https://twitter.com/ReWorkhub1" style="padding: 10px 10px 5px;"><img src="http://cleancode.me/test/email-images/twitter.png" alt="twitter" style="height: 40px; width: 40px;" /></a></th>
+                                                                            <th><a href="https://t.me/ReWorkHUB" style="padding: 10px 10px 5px;"><img src="http://cleancode.me/test/email-images/telegram.png" alt="telegram" style="height: 40px; width: 40px;" /></a></th>
+                                                                        </tr>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </center>
+
+                                                </th>
+                                            </tr>
+                                        </table>
+                                    </th>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </center>
+        </td>
+    </tr>
+</table>
+</body>
+
+</html>
