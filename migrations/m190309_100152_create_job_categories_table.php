f6962bbd5a038d5e47b679d1318abef46ae8d30c
trad3r <tatusr@gmail.com>
2020-03-26 12:48:25 +0300

    добавлены данные для некоторых таблиц

--- migrations/m190309_100152_create_job_categories_table.php
+++ migrations/m190309_100152_create_job_categories_table.php
@@ -24,6 +24,21 @@ class m190309_100152_create_job_categories_table extends Migration
         ], $tableOptions);
 
         $this->createIndex('idx-job-categories-category', '{{%job_categories}}', 'category');
+        $this->batchInsert('{{%job_categories}}', ['category'], [
+            ['HR'],
+            ['IT'],
+            ['Блокчейн'],
+            ['Дизайн, анимация, графика'],
+            ['Маркетинг, Реклама'],
+            ['Медиа'],
+            ['Образование'],
+            ['Переводы'],
+            ['Поддержка, админ-ая деятельность'],
+            ['Продажи'],
+            ['Тексты и контент'],
+            ['Финансы, Учет, Аудит'],
+            ['Фото и видео'],
+        ]);
     }
 
     /**
