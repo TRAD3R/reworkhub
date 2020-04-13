ea979f5fc20c46688500f5d1e41754ce6ac8f22e
trad3r <tatusr@gmail.com>
2020-04-13 12:31:19 +0300

    Удален токен из параметров

--- config/params.php
+++ config/params.php
@@ -4,9 +4,6 @@ return [
     'adminEmail' => '',
     'supportEmail' => '',
     'user.passwordResetTokenExpire' => 3600,
-    'telegram' => [
-        'botTokenCV' => '816998441:AAHX6P8k79zCLNsFdiH-vnGvaRNNsnvtSWM'
-    ],
     'tariffs' => [
         [
             'type' => '',
