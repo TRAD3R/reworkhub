e4575e7ad58b8d4021f60a306b2d5e694c74b23e
Daniil <zeveres88@gmail.com>
2020-05-25 19:38:35 +0300

    контент на странице rules вставлен код метрики Яндекс вставлен код чата Replain

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -50,6 +50,27 @@ AppAsset::register($this);
     <!-- Custom Browsers Color End -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript" >
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(63634387, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+      });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/63634387" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->
+  <script>
+      window.replainSettings = { id: '5cc1131b-5909-41f7-86a5-41326983187d' };
+      (function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
+          var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+      })('https://widget.replain.cc/dist/client.js');
+  </script>
 </head>
 <style>
   html body #__replain_widget_iframe path.accent-color-fill {
